a99a0893e6be39b62bfd046e36bec625dddc5c0a
Marcel Ferreira Martins <doiscontroles25@gmail.com>
2026-03-11 15:52:30 -0300

    Add files via upload

--- Estoque.py
+++ Estoque.py
@@ -22,12 +22,11 @@ authenticator = stauth.Authenticate(
     cookie_expiry_days=30
 )
 
-if "authentication_status" in st.session_state:
-    if st.session_state["authentication_status"] and st.session_state.get("username") not in credentials["usernames"]:
-        st.session_state.clear()
-
-# MOSTRAR LOGIN
-authenticator.login(location="main")
+try:
+    authenticator.login(location="main")
+except KeyError:
+    st.session_state.clear()
+    st.rerun()
 
 
 # PEGAR STATUS
